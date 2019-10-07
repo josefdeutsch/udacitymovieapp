770c4a166da92968fee9195e1e435b67c3352c68
philippdeutsch <philippdeutsch@icloud.com>
2019-10-07 03:50:34 -0700

    REFACTORED

--- app/src/main/java/com/example/myapplication/Config.java
+++ app/src/main/java/com/example/myapplication/Config.java
@@ -1,8 +1,7 @@
 package com.example.myapplication;
 
-import org.jetbrains.annotations.NotNull;
-
 public class Config {
+
 
     public static final String YoutubeAPIKEY = "";
     public static final String MDBAPIKEY = "";
@@ -12,7 +11,6 @@ public class Config {
    public static final String POPULARURL = "https://api.themoviedb.org/3/movie/popular?api_key="+MDBAPIKEY+"&language=en-US&page=1";
    public static final String TOPRATEDURL = "https://api.themoviedb.org/3/movie/top_rated?api_key="+MDBAPIKEY+"&language=en-US&page=1";
    public static final String MOVIEDETAILSURL = "https://api.themoviedb.org/3/movie/?api_key="+MDBAPIKEY+"&language=en-US";
-
 
     public static final int POPULAR = 0;
     public static final int TOP_RATED = 1;
